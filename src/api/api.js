c4c393c2ce1acd8ef5fdec554e9e99403ca19099
Marco Matildo <mmatildo98@gmail.com>
2023-08-29 20:44:14 +0800

    added singleProduct

---
+++ src/api/api.js
@@ -0,0 +1,9 @@
+const API = {
+    products: "https://fakestoreapi.com/products",
+    singleProduct: "https://fakestoreapi.com/products/",
+    categorioes: "https://fakestoreapi.com/products/categories",
+    singleCategory: "https://fakestoreapi.com/products/category/"
+
+  };
+
+export default API
